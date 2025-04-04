f29f2947e014d22e033ad0ef8c6ef0333865af57
jashwant-jaladi <jasshwant.jvs@protonmail.com>
2025-04-05 01:58:11 +0530

    Initial commit: Vite + React + Styled Components + Capacitor setup

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Arial', sans-serif;
+    background: #fff;
+    color: #202124;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    background: none;
+  }
+
+  input {
+    outline: none;
+  }
+`;
