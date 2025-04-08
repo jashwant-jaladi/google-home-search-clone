6aba72ceaf699c1c0b058f39851ff4d0b6ff9113
jashwant-jaladi <jasshwant.jvs@protonmail.com>
2025-04-08 23:02:29 +0530

    designed iconrow component

--- src/components/IconRow.tsx
+++ src/components/IconRow.tsx
@@ -1,7 +1,32 @@
+import { IconRowContainer, IconRowItem } from '../styles/IconRow.styles'
+import { MdOutlineImageSearch } from "react-icons/md";
+import { IoLanguage } from "react-icons/io5";
+import { RiGraduationCapLine } from "react-icons/ri";
+import { IoMdMusicalNote } from "react-icons/io";
+
 
 const IconRow = () => {
   return (
-    <div>IconRow</div>
+    <>
+<IconRowContainer>
+  <IconRowItem bgColor="#5C4612">
+    <MdOutlineImageSearch size={24} color="#FABB05" />
+  </IconRowItem>
+  
+  <IconRowItem bgColor="#1A2B50">
+    <IoLanguage size={24} color="#4285F4" />
+  </IconRowItem>
+  
+  <IconRowItem bgColor="#1B3D33">
+    <RiGraduationCapLine size={24} color="#0F9D58" />  
+  </IconRowItem>
+  
+  <IconRowItem bgColor="#522726">
+    <IoMdMusicalNote size={24} color="#E94335" />
+  </IconRowItem>
+</IconRowContainer>
+
+    </>
   )
 }
 
---
+++ src/styles/IconRow.styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const IconRowContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  margin-top: 20px;
+  width:100%
+`;
+export const IconRowItem = styled.button<{ bgColor: string }>`
+  border: none;
+  border-radius: 24px;
+  width: 90px;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background-color: ${({ bgColor }) => bgColor};
+  box-shadow: inset 0 0 2px rgba(255,255,255,0.1);
+  
+`;
