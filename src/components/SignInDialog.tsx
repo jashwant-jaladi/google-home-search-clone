f692bd18ead315afbe024e20ccebc59616dc50ea
jashwant-jaladi <jasshwant.jvs@protonmail.com>
2025-04-11 18:28:45 +0530

    removed useeffect from SignInDialog

--- src/components/SignInDialog.tsx
+++ src/components/SignInDialog.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useEffect } from 'react';
+
 import { FcGoogle } from "react-icons/fc";
 import { auth } from '../firebase';
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
